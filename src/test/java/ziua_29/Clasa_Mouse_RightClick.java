a57a86055aa9d73cea12158d7a1b5fa11e05acb7
Cristian Zidarescu <Cristian Zidarescu>
2024-05-16 11:21:06 +0300

    Proiect-Selenium - curs in limba romana - revizia 16.05.2024

--- src/test/java/ziua_29/Clasa_Mouse_RightClick.java
+++ src/test/java/ziua_29/Clasa_Mouse_RightClick.java
@@ -1,3 +1,7 @@
+/*
+Cum facem un click dreapta si cum il initializam.
+Right click - contextClick(element)
+ */
 package ziua_29;
 
 import drivers.DriverManager;
@@ -5,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 import java.util.Set;
@@ -17,7 +22,7 @@ public class Clasa_Mouse_RightClick extends DriverManager {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds((10)));
         driver.manage().window().maximize();
 
-        driver.get("https://swisnl.github.io/JQuery-contextMenu/demo.html/");
+        driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
 
 //------------------------------------------- partea de focalizare pe pagina -------------------------------------------
         String parentWindow = driver.getWindowHandle();
@@ -29,15 +34,29 @@ public class Clasa_Mouse_RightClick extends DriverManager {
                 break;
             }
         }
-
 //  ----------------------------------- aici incepem sa scriem programul -----------------------------------------------
+/*
+    Right click - contextClick(elementul_apasat).perform()
+*/
 
 //  Cautam elementul "BUTTON" pe care trebuie sa facem click dreapta :
-        WebElement button = driver.findElement(By.xpath(""));
+        WebElement butonul = driver.findElement(By.xpath("/html/body/div/section/div/div/div/p/span"));
+//                                   By.xpath -ul din exercitiu este //span[@class='content-menu-one btn btn-neutral']
+        Thread.sleep(500);
+        Actions actiune = new Actions(driver);
 
+/*  Dam click dreapta - RIGHT CLICK, este doar o ACTIUNE , deci ea doar genereaza actiunea nu o si initializeaza.
+Pentru a realiza actiunea se adauga linia unde se defineste "driver.findElement" si ii punem actiunea click normal
+ */
+        actiune.contextClick(butonul).perform();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("/html/body/ul/li[3]")).click();
 
+        Thread.sleep(2500); // se pune doar ca sa vedem fereastra care apare.
+        driver.switchTo().alert().accept(); //  inchidem fereastra de alerta.
+        Thread.sleep(500);
 
-    Thread.sleep(5000);
+    Thread.sleep(3500);
     driver.quit();
 
     }
